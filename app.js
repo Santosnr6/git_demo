d8f7bf7094834970cb3bccd7df312e1543c84bc8
Jesper Nyberg <91jesnyb@gafe.molndal.se>
2024-06-02 21:45:56 +0200

    done!

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 console.log('hello world');
+
+window.addEventListener('load', function() {
+    console.log('window loaded');
+});
+
+global.name = 'John Doe';
+
+function getName() {
+    return global.name;
+}
+
+function setName(newName) {
+    global.name = newName;
+}
+
