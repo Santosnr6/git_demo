23e671fe3e4acfa89d61ddb6ea9e03bc5c66b686
Jesper Nyberg <91jesnyb@gafe.molndal.se>
2024-06-03 09:46:22 +0200

    person

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 console.log('Hello, World!');
+
+window.addEventListener('load', function() {
+    console.log('The page has fully loaded!');
+});
+
+let person = 'Jesper';
+
+function getName() {
+    return person;
+}
+
+function setName(newName) {
+    person = newName;
+}
